10244f3bd603744bbe49acaecce6b285e5084be0
Luiza Andrade <luiza.c.andrade@gmail.com>
2023-02-21 09:47:55 -0600

    fix issues with repeated observations

--- code/simulate-child.R
+++ code/simulate-child.R
@@ -29,20 +29,23 @@ children <-
     ),
     mother_index = add_level(
       N = n_mothers,
-      mother_age = runif(N, min = 15, max = 50) %>% round(0),
+      mother_age = runif(N, min = 17, max = 50) %>% round(0),
       mother_yob = 2022 - mother_age,
-      mother_educ = rnorm(N, mean = 9, sd = 3.5) %>% round(0),
+      mother_educ = rnorm(N, mean = 9, sd = 3.5) %>% round(0) %>% abs,
+      mother_income = (rpois(N, lambda = 2) * 100) + ((runif(N, min = 0, max = 9)  %>% round(0)) * 10),
       n_children = rpois(N, lambda = 2),
       flag_m_year = rbinom(N, 1, prob = .05),
       flag_m_age = rbinom(N, 1, prob = .1),
       flag_n_age = runif(N, min = -5, max = 5) %>% round(0),
       flag_m_children = rbinom(N, 1, prob = .05),
-      flag_n_children = runif(N, min = -1, max = 2) %>% round(0)
+      flag_n_children = runif(N, min = -1, max = 2) %>% round(0),
+      flag_i_high = rbinom(N, 1, prob = .05),
+      flag_i_zero = ifelse(!flag_i_high, rbinom(N, 1, prob = .2), 0),
+      flag_i_neg = ifelse(!flag_i_high & !flag_i_zero, rbinom(N, 1, prob = .1), 0)
     ),
     child_index = add_level(
       N = n_children,
-      age = runif(N, min = 2, max = 15) %>% round(0),
-      yob = 2022 - age,
+      age = runif(N, min = 0, max = 13) %>% round(0),
       sex = runif(N, min = 1, max = 6) %>% round(0) %>% 
         factor(
           levels = 1:6,
@@ -52,33 +55,43 @@ children <-
       in_school = ifelse(
         age >= 6, 
         rbinom(N, 1, prob = .9), 
-        rbinom(N, 1, prob = .4)
+        rbinom(N, 1, prob = .2)
       ),
-      grade = ifelse(in_school, age - 5, NA),
+      grade = ifelse(in_school, age - 5, NA) %>% abs(),
       flag_c_year = rbinom(N, 1, prob = .05),
       flag_c_grade = rbinom(N, 1, prob = .02),
       flag_n_grade = rpois(N, lambda = 3),
-      flag_c_grade_dk = rbinom(N, 1, prob = .05)
+      flag_c_grade_dk = rbinom(N, 1, prob = .05),
+      yob = ifelse(flag_c_year, 9999, 2022 - age)
     )
   ) %>%
   mutate(
     mother_yob = ifelse(flag_m_year, -99, mother_yob),
     mother_age = ifelse(flag_m_age, mother_age + flag_n_age, mother_age),
     n_children = ifelse(flag_m_children, n_children + flag_n_children, n_children),
-    yob = ifelse(flag_c_year, 9999, yob),
     grade = ifelse(flag_c_grade, grade + flag_n_grade, grade),
-    grade = ifelse(flag_c_grade_dk, NA, grade)
+    grade = ifelse(flag_c_grade_dk, -99, grade),
+    grade = na_if(grade, 0),
+    mother_income = 
+      case_when(
+        flag_i_high == 1 ~ mother_income * 20,
+        flag_i_zero == 1 ~ 0,
+        flag_i_neg == 1 ~ -88,
+        TRUE ~ mother_income
+      )
   ) %>%
   select(
     -n_mothers,
     -starts_with("flag")
-  )
+  ) 
 
+
+  
 # Create mother-level tidy data ------------------------------------------------
 
 mother_tidy <-
   children %>%
-  select(-c(child_index:grade)) %>%
+  select(-c(child_index:yob)) %>%
   unique %>%
   group_by(village_id) %>%
   transmute(
@@ -90,6 +103,7 @@ mother_tidy <-
     yob = mother_yob,
     age = mother_age,
     educ = mother_educ,
+    mother_income,
     n_children,
     mother_index
   ) %>%
@@ -183,6 +197,7 @@ child_tidy %>%
 
 # Create mother-level wide data ------------------------------------------------
 
+
 mother_raw <-
   child_tidy %>%
   select(-c(child_id, village_id)) %>%
@@ -196,7 +211,36 @@ mother_raw <-
     by = "mother_id"
   )
 
-mother_raw %>%
+dups <- 
+  mother_raw %>%
+  slice_sample(n = 3)
+
+missing_dup <- 
+  dups %>%
+  slice(1) %>%
+  select(village_id:n_children)
+
+all_dups <-
+  dups %>%
+  slice(-1)
+
+mother_w_dups <-
+  mother_raw %>%
+  mutate(
+    across(
+      mother_id,
+      ~ ifelse(
+        . == "1003",
+        "0103",
+        .
+      )
+    )
+  ) %>%
+  bind_rows(missing_dup) %>%
+  bind_rows(all_dups) %>%
+  arrange(mother_id)
+
+mother_w_dups %>%
   select(-mother_index) %>%
   write_csv(
     here(
@@ -205,6 +249,18 @@ mother_raw %>%
       "mother_survey.csv"
     )
   )
+
+mother_w_dups %>%
+  select(-mother_index) %>%
+  write_dta(
+    here(
+      "data",
+      "raw",
+      "mother_survey.dta"
+    )
+  )
+
+
 
 # Create example of long data --------------------------------------------------
 
